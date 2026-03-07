636102ee5956dd5cbc10145b30fd231b5401cd25
david03-ops <david.josepha03@gmail.com>
2026-03-07 18:08:45 -0500

    check-rows funct (pthread may be in wrong spot)

--- main.c
+++ main.c
@@ -12,7 +12,28 @@ typedef struct {
 } parameters;
 
 // Check Rows
+void *check_rows(void *param)
+{
+    int i;
+    int j;
 
+    for (i = 0; i < 9; i++) {
+        int seen[10] = {0};
+
+        for (j = 0; j < 9; j++) {
+            int num = board[i][j];
+            //check if valid
+            if (num < 1 || num > 9 || seen[num]) {
+                result[0] = 0;
+                pthread_exit(0);
+            }
+            seen[num] = 1;
+        }
+    }
+
+    result[0] = 1; // All rows = valid
+    pthread_exit(0);
+}
 
 // Check Columns
 

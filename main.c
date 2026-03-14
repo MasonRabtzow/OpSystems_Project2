cd4c2c55376154328cfda3028d0c7b1d9e66ae2f
MasonRabtzow <rabtzow.mason@gmail.com>
2026-03-14 12:56:43 -0400

    Made some formatting changes and added comments

--- main.c
+++ main.c
@@ -24,11 +24,13 @@ void *check_rows(void *param)
     int i;
     int j;
 
+    //checks each row by iterating through each row and checking for 1-9 and ducplicates
     for (i = 0; i < 9; i++) {
         int seen[10] = {0};
 
         for (j = 0; j < 9; j++) {
             int num = board[i][j];
+
             //check if valid
             if (num < 1 || num > 9 || seen[num]) {
                 result[0] = 0;
@@ -48,6 +50,7 @@ void *check_cols(void *param)
     int i;
     int j;
 
+    //iterates through each column and checks for 1-9 and duplicates
     for (i = 0; i < 9; i++) {
         int check[10] = {0};
 
@@ -77,16 +80,16 @@ void *check_3x3(void *param)
 
     int check[10] = {0};
 
+    //iterates through 1 3x3 box and checks for 1-9 and duplicates
     for (int i = row; i < row + 3; i++) {
-        
         for (int j = col; j < col + 3; j++) {
             int num = board[i][j];
+
             //check if valid
             if (num < 1 || num > 9 || check[num]) {
                 result[index] = 0;
                 pthread_exit(0);
             }
-            
             check[num] = 1;
         }
     }
@@ -105,6 +108,7 @@ void *check_row_thread(void *param)
 
     int check[10] = {0};
 
+    //checks 1 row for 1-9 and duplicates
     for (int j = 0; j < 9; j++) {
         int num = board[row][j];
         //check if valid
@@ -119,6 +123,9 @@ void *check_row_thread(void *param)
     pthread_exit(0);
 }
 
+
+
+
 // Check 1 Column (Mode 2)
 void *check_col_thread(void *param)
 {
@@ -129,6 +136,7 @@ void *check_col_thread(void *param)
 
     int check[10] = {0};
 
+    //checks 1 column for 1-9 and duplicates
     for (int i = 0; i < 9; i++) {
         int num = board[i][col];
         //check if valid
@@ -152,6 +160,7 @@ int main(int argc, char *argv[]){
 
     int mode = atoi(argv[1]);
 
+    // Read board from file
     for(i = 0; i < 81; i++){
         fscanf(f, "%d", &board[i / 9][i % 9]);
     }
@@ -159,6 +168,7 @@ int main(int argc, char *argv[]){
 
     fclose(f);
 
+    //Print board
     printf("BOARD STATE IN input.txt:\n");
     for(i = 0; i < 81; i++){
         printf("%d ", board[i / 9][i % 9]);
@@ -170,7 +180,9 @@ int main(int argc, char *argv[]){
     // Start system timer
     clock_t start = clock();
 
+    //Mode 1: 11 threads (1 for rows, 1 for columns, 9 for 3x3 boxes)
     if(mode == 1){
+        //create threads for rows, columns, and 3x3 boxes
         pthread_t threads[11];
         pthread_create(&threads[t], NULL, check_rows, NULL);
         t++;
@@ -179,6 +191,7 @@ int main(int argc, char *argv[]){
 
         for(int r = 0; r < 9; r += 3){
             for(int c = 0; c < 9; c += 3){
+                //make struct for 3x3 box parameters and create thread
                 params *p = malloc(sizeof(params));
                 p->row = r;
                 p->col = c;
@@ -189,11 +202,16 @@ int main(int argc, char *argv[]){
             }
         }
 
+        //join all threads
         for(i = 0; i < 11; i++){
             pthread_join(threads[i], NULL);
         }
-    } else if(mode == 2){
+
+    //Mode 2: 27 threads (9 for rows, 9 for columns, 9 for 3x3 boxes)
+    }else if(mode == 2){
         pthread_t threads[27];
+
+        //make struct for each row
         for(i = 0; i < 9; i++){
             params *p_row = (params*) malloc(sizeof(params));
             p_row->row = i;
@@ -201,6 +219,8 @@ int main(int argc, char *argv[]){
             pthread_create(&threads[t], NULL, check_row_thread, (void*) p_row);
             t++;
         }
+
+        //make struct for each column
         for(i = 0; i < 9; i++){
             params *p_col = (params*) malloc(sizeof(params));
             p_col->col = i;
@@ -209,6 +229,7 @@ int main(int argc, char *argv[]){
             t++;
         }
 
+        //make struct for each 3x3 box
         for(int r = 0; r < 9; r += 3){
             for(int c = 0; c < 9; c += 3){
                 params *p = malloc(sizeof(params));
@@ -221,10 +242,13 @@ int main(int argc, char *argv[]){
             }
         }
 
+        //join all threads
         for(i = 0; i < 27; i++){
             pthread_join(threads[i], NULL);
         }
-    } else {
+
+    //if mode is not 1 or 2, print error and exit
+    }else{
         printf("Invalid mode. Use 1 for full board check or 2 for row/column check.\n");
         fclose(f);
         return 1;
@@ -233,8 +257,8 @@ int main(int argc, char *argv[]){
     // Confirm result
     int correct = 1;
 
-    for (int i = 0; i < t; i++) {
-        if (result[i] == 0) {
+    for (int i = 0; i < t; i++){
+        if(result[i] == 0){
             correct = 0;
             break;
         }
